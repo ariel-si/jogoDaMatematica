5b54afe7a0037df0449ead0b5387bc2c0e8ac25a
ariel montovani <am.si@outlook.com>
2019-11-07 19:41:58 -0300

    metodos geraNumerosAleatoris, soma e verifica criados

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,16 +2,50 @@ package com.example.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+import android.widget.TextView;
 import com.example.myapplication.R;
+import java.util.Random;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  public int n1;
+  public int n2;
+  public int resultado;
+  public int resposta;
 
+  public void geraNumeroAleatorio() {
+
+    Random rd = new Random();
+
+    n1 = rd.nextInt(10);
+    n2 = rd.nextInt(10);
+  }
+
+  public void soma(int v1, int v2) {
+
+    TextView pergunta = (TextView) findViewById(R.id.txtQuestao);
+    pergunta.setText(Integer.toString(v1) + " + " + Integer.toString(v2) + " =");
+  }
+
+  public void verifica() {
+
+    TextView txtresultado = (TextView) findViewById(R.id.txtResultadoCorreto);
+
+    resposta = n1 + n2;
+
+    if (resposta == resultado) {
+      txtresultado.setText(Integer.toString(resultado));
+    }else{
+      //linpa caixa de respostas, diminuir score
     }
+  }
 
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    geraNumeroAleatorio();
+    soma(n1, n2);
+  }
 }
