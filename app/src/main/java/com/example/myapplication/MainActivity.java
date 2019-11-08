c8774a8d7bcadb5da0ec9eb8fea39779c18874db
ariel montovani <am.si@outlook.com>
2019-11-08 11:27:42 -0300

    finalizando acao e versao de teste

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,41 +2,62 @@ package com.example.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import com.example.myapplication.R;
 import java.util.Random;
 
 public class MainActivity extends Activity {
 
+  public TextView txtresultado, txtpergunta;
+  public EditText edtreaposta;
+  public Button btnverifica;
+
   public int n1;
   public int n2;
   public int resultado;
-  public int resposta;
+  public String resposta;
 
   public void geraNumeroAleatorio() {
+  
+    edtreaposta = (EditText) findViewById(R.id.edtResposta);
 
     Random rd = new Random();
 
-    n1 = rd.nextInt(10);
-    n2 = rd.nextInt(10);
+    n1 = rd.nextInt(9);
+    n2 = rd.nextInt(9);
+    
+    edtreaposta.setText("");
   }
 
   public void soma(int v1, int v2) {
 
-    TextView pergunta = (TextView) findViewById(R.id.txtQuestao);
-    pergunta.setText(Integer.toString(v1) + " + " + Integer.toString(v2) + " =");
+    txtpergunta = (TextView) findViewById(R.id.txtQuestao);
+    txtpergunta.setText(Integer.toString(v1) + " + " + Integer.toString(v2) + " =");
   }
 
   public void verifica() {
+    resposta = edtreaposta.getText().toString();
+    
+    txtresultado = findViewById(R.id.txtResultadoCorreto);
 
-    TextView txtresultado = (TextView) findViewById(R.id.txtResultadoCorreto);
+    resultado = n1 + n2;
+    int res = Integer.parseInt(resposta);
 
-    resposta = n1 + n2;
+    if (res == resultado) {
+    
+      txtresultado.setText("Acertou! resposta correta é: "+resultado);
+      
+      geraNumeroAleatorio();
+      soma(n1, n2);
+    } else {
 
-    if (resposta == resultado) {
-      txtresultado.setText(Integer.toString(resultado));
-    }else{
-      //linpa caixa de respostas, diminuir score
+      txtresultado.setText("errou! resposta correta é: " + resultado);
+     
+      geraNumeroAleatorio();
+      soma(n1, n2);
     }
   }
 
@@ -44,6 +65,16 @@ public class MainActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    btnverifica = findViewById(R.id.btnVerifica);
+    btnverifica.setOnClickListener(
+        new View.OnClickListener() {
+
+          @Override
+          public void onClick(View v) {
+            verifica();
+          }
+        });
 
     geraNumeroAleatorio();
     soma(n1, n2);
